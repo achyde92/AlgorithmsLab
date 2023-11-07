e25ac96032d63414608663f6cb5ca6a7b89b20c6
Alex Hyde <alexhyde@Alexs-MacBook-Pro.local>
2023-11-07 17:22:32 -0500

    Added first 2 task

--- AlgoBigOLab/AlgoBigOLab/Algorithms.cs
+++ AlgoBigOLab/AlgoBigOLab/Algorithms.cs
@@ -1,11 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace AlgoBigOLab
 {
-	public class Algorithms
-	{
-		public Algorithms()
-		{
-		}
-	}
-}
+    static class Algorithms
+    {
+        // Should be constant. Input never changes. O(1) 
+        public static bool OddorEven(int RandomNumber)
+        {
+            Random rand = new Random();
+            RandomNumber = rand.Next(1, 20);
 
+            if (RandomNumber % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool LessThan100(List<int> RandomList)
+        {
+            RandomList = new List<int>()
+            {
+                70,
+                65,
+                99,
+                110
+            };
+          
+            for (int i = 0; i < RandomList.Count; i++)
+            {
+                if (RandomList[i] >= 100)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        ////public static bool DuplicateNames(List<string> name)
+        ////{
+        ////    List<string> names = new List<string> { "Tron", "Alex", "Bob", "Alex", "Chuck" };
+
+        ////    HashSet<string> NoDupes = new HashSet<string>();
+
+        ////    foreach (string name in names)
+        ////    {
+
+        ////    }
+        //}
+    }
+}
